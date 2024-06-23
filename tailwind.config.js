0db36b0c19520e4ea9a8e28c0ce158ab2e10c177
ARFprince <parmanurrahmanfaisal@gmail.com>
2024-06-24 03:51:47 +0600

    starting

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import daisyui from "daisyui";
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui,
+  ],
+}
